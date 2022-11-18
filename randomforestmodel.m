7cb771f9647275411ae53ee9643bf35050f1c444
Nathan <nnewbury9469@gmail.com>
2022-11-18 12:10:38 -0700

    test
    test

---
+++ randomforestmodel.m
@@ -0,0 +1,25 @@
+%% test
+filename = 'weatherHistory.csv'
+
+opts = detectImportOptions(filename);
+opts.Delimiter = {','};
+opts.VariableTypes={'char','char','char','double','double','double','double','double','double','double','double','char'}
+T = readtable(filename,opts);
+X = [T.Var4]% T.Humidity];
+% split into weeks
+timearr = cell2mat(T.Var1);
+time = datetime(timearr(:,1:19));
+%index = find((day(time)==1)&(hour(time)==0));
+timeinc = 720;
+numtimeinc = 100
+bigarr = zeros(numtimeinc,timeinc);
+Y = zeros(numtimeinc,1);
+for  i = 1:numtimeinc
+    bigarr(i,:)= X((i -1)*(timeinc)+1:i*timeinc);
+    Y(i) = mean(X((i-1)*(timeinc)+1:i*timeinc));
+end
+
+%Y = zeros(size(X,1),1)+ mean(T.WindSpeed_km_h_);
+%T = readtable('myfile.csv','NumHeaderLines',3);
+
+Mdl = TreeBagger(8,bigarr,Y, 'Method', 'regression');
