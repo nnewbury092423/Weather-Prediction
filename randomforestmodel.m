cf3087260ecff2c7026bd537c7dea13b32abcd1e
Nathan <nnewbury9469@gmail.com>
2022-11-20 17:14:41 -0700

    erar
    test

--- randomforestmodel.m
+++ randomforestmodel.m
@@ -1,22 +1,35 @@
 %% test
+clear all
 filename = 'weatherHistory.csv'
 
 opts = detectImportOptions(filename);
 opts.Delimiter = {','};
 opts.VariableTypes={'char','char','char','double','double','double','double','double','double','double','double','char'}
 T = readtable(filename,opts);
-X = [T.Var4]% T.Humidity];
+X = [T.Var7]% T.Humidity];
 % split into weeks
 timearr = cell2mat(T.Var1);
 time = datetime(timearr(:,1:19));
 %index = find((day(time)==1)&(hour(time)==0));
-timeinc = 720  ;
-numtimeinc = 100
-bigarr = zeros(numtimeinc,timeinc);
+timeinc = 2000 ;
+numtimeinc = floor(numel(X)/timeinc)- 1;
+bigarr = zeros(numtimeinc,2000);
 Y = zeros(numtimeinc,1);
+lev = 3;
+wname = 'db1';
+nbcol = 64
+
+
+%[C1,L1] = wavedec(X,1,'db1');
+%[C2,L2] = wavedec(X,2,'db1');
+
+
 for  i = 1:numtimeinc
-    bigarr(i,:)= X((i -1)*(timeinc)+1:i*timeinc);
-    Y(i) = mean(X((i-1)*(timeinc)+1:i*timeinc));
+    %C3 = X((i -1)*(timeinc)+1:i*timeinc);
+    [C3,L3] = wavedec(X((i -1)*(timeinc)+1:i*timeinc),3,'db1');
+     [cd1,cd2,cd3] = detcoef(C3,L3,[1 2 3]);
+    bigarr(i,:)= C3;
+    Y(i) = mean(X(i*timeinc:i*timeinc+24));
 end
 
 %Y = zeros(size(X,1),1)+ mean(T.WindSpeed_km_h_);
@@ -26,8 +39,8 @@ end
 
 
 
-Mdl = TreeBagger(100,bigarr,Y, 'Method', 'regression',OOBPrediction="on",NumPredictorsToSample=50);
-
+    Mdl = TreeBagger(500,bigarr,Y, 'Method', 'regression','OOBPrediction','on','NumPredictorsToSample',50);
+figure
 plot(oobError(Mdl))
 xlabel('Number of Trees')
 ylabel('MSE Error')
